10a98112b265b7274a2af9e7c9fc113696bcd177
jinweihuang20 <jinweihuang1020@gmail.com>
2024-09-03 21:43:39 +0800

    Init commit

---
+++ Evaluation/EvaluatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AGVSystem.Evaluation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AGVSystemCommonNet6.Configuration;
+using EquipmentManagment.Manager;
+
+namespace AGVSystem.Evaluation.Tests
+{
+    [TestClass()]
+    public class EvaluatorTests
+    {
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            AGVSConfigulator.Init();
+
+            StaEQPManagager.InitializeAsync(new EquipmentManagment.MainEquipment.clsEQManagementConfigs()
+            {
+                EQConfigPath = $"{AGVSConfigulator.SysConfigs.EQManagementConfigs.EquipmentManagementConfigFolder}//EQConfigs.json",
+                WIPConfigPath = $"{AGVSConfigulator.SysConfigs.EQManagementConfigs.EquipmentManagementConfigFolder}//WIPConfigs.json",
+                ChargeStationConfigPath = $"{AGVSConfigulator.SysConfigs.EQManagementConfigs.EquipmentManagementConfigFolder}//ChargStationConfigs.json",
+
+            });
+        }
+
+        [TestMethod()]
+        public void EvaluatorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StartTest()
+        {
+            EvaluateFactory.StartTest().GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ Evaluation/KeyFrameTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AGVSystem.Evaluation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EquipmentManagment.MainEquipment;
+
+namespace AGVSystem.Evaluation.Tests
+{
+    [TestClass()]
+    public class KeyFrameTests
+    {
+        [TestMethod()]
+        public void KeyFrameTest()
+        {
+            var keyFrame = new KeyFrame();
+            keyFrame.EQControls.Add(new EQControl()
+            {
+                EQ = new clsEQ(new EquipmentManagment.Device.Options.clsEndPointOptions()),
+                LdUldState = EQControl.LDULD_STATE.UNLOAD
+            });
+            keyFrame.EQControls.Add(new EQControl()
+            {
+                EQ = new clsEQ(new EquipmentManagment.Device.Options.clsEndPointOptions()),
+                LdUldState = EQControl.LDULD_STATE.NO_REQUEST
+            });
+
+            var keyFrame2 = new KeyFrame(TimeSpan.FromSeconds(10));
+
+            Console.WriteLine(keyFrame.Time);
+            Console.WriteLine(keyFrame2.Time);
+        }
+    }
+}
