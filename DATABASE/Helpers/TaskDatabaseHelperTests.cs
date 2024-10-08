7c964e880da54ba162a62a67c3e4c74c1a94203e
jinweihuang20 <jinweihuang1020@gmail.com>
2024-10-09 01:12:03 +0800

    新增單元測試

---
+++ DATABASE/Helpers/TaskDatabaseHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AGVSystemCommonNet6.DATABASE.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AGVSystemCommonNet6.Alarm;
+
+namespace AGVSystemCommonNet6.DATABASE.Helpers.Tests
+{
+    [TestClass()]
+    public class TaskDatabaseHelperTests
+    {
+        [TestMethod()]
+        public void CheckAndRebuildFailReasonTest()
+        {
+            AlarmManagerCenter.Initialize();
+            TaskDatabaseHelper.CheckAndRebuildFailReason($"[1063] UNLOAD_BUT_AGV_NO_CARGO_MOUNTED(UNLOAD_BUT_AGV_NO_CARGO_MOUNTED)");
+        }
+    }
+}
