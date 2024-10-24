79885beda316af3c0e29786829e46f2b23c5e569
jinweihuang20 <jinweihuang1020@gmail.com>
2024-10-24 16:39:21 +0800

    新增充電站溫控模組實作/

---
+++ Device/TemperatureModuleDevice/E5DC800Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquipmentManagment.Device.TemperatureModuleDevice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentManagment.Device.TemperatureModuleDevice.Tests
+{
+    [TestClass()]
+    public class E5DC800Tests
+    {
+        [TestMethod()]
+        public void ParseTemperatureTest()
+        {
+            E5DC800 module = new E5DC800(new TemperatureModuleAbstract.TemperatureModuleSetupOptions());
+            double _temperature = module.ParseTemperature(new byte[] { 0x00, 0x00, 0x00, 0x1F });
+            Assert.AreEqual(31, _temperature);
+            double _temperature2 = module.ParseTemperature(new byte[] { 0x00, 0x01, 0x01, 0x1F });
+            Assert.AreEqual(287, _temperature2);
+        }
+    }
+}
---
+++ Device/TemperatureModuleDevice/TemperatureModuleAbstractTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquipmentManagment.Device.TemperatureModuleDevice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentManagment.Device.TemperatureModuleDevice.Tests
+{
+    [TestClass()]
+    public class TemperatureModuleAbstractTests
+    {
+        [TestMethod()]
+        public void ConnectTestTest()
+        {
+            TemperatureModuleAbstract module = new E5DC800(new TemperatureModuleAbstract.TemperatureModuleSetupOptions
+            {
+                Protocol = TemperatureModuleAbstract.TemperatureModuleSetupOptions.COMMUNICATION_PROTOCOL.SERIAL_PORT,
+                COM = "COM30",
+                BaudRate = 9600
+            });
+            bool connected = module.ConnectTest().GetAwaiter().GetResult();
+            module.serialPort.Dispose();
+            Assert.IsTrue(connected);
+        }
+    }
+}
