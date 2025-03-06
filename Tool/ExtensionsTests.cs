12344c6ac9fa277289299e69b7b461d67d108acc
jinweihuang20 <jinweihuang1020@gmail.com>
2025-03-06 22:31:18 +0800

    修改PMBus用數值轉換擴充功能與新增單測

---
+++ Tool/ExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquipmentManagment.Tool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentManagment.Tool.Tests
+{
+    [TestClass()]
+    public class ExtensionsTests
+    {
+        [TestMethod()]
+        public void DoubleToLinear11V2Test()
+        {
+            double fv = 27.1;
+
+            byte[] dataBytes = fv.DoubleToLinear11V2(-2);
+
+            double doubleValConverted = dataBytes.Linear11ToDouble(-2);
+
+            double error = Math.Abs(fv - doubleValConverted);
+            Console.WriteLine($"error : {error}");
+
+            Assert.IsTrue(error <= 0.11);
+        }
+    }
+}
