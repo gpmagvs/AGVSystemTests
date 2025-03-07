00c9aa4fd56daf6162048fe9bca5a59f14397ac2
jinweihuang20 <jinweihuang1020@gmail.com>
2025-03-07 08:38:36 +0800

    修改充電器數據讀取流程與單元測試

---
+++ ChargeStation/clsChargeStationTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquipmentManagment.ChargeStation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AGVSystemCommonNet6;
+
+namespace EquipmentManagment.ChargeStation.Tests
+{
+    [TestClass()]
+    public class clsChargeStationTests
+    {
+        [TestMethod()]
+        public void ReadChargerTest()
+        {
+            clsChargeStationGY7601Base charger = new clsChargeStationGY7601Base(new clsChargeStationOptions()
+            {
+                ConnOptions = new Connection.ConnectOptions
+                {
+                    IP = "192.168.0.181",
+                    Port = 10002,
+                    ConnMethod = Connection.CONN_METHODS.TCPIP,
+                }
+            });
+
+            bool connected = charger.Connect().GetAwaiter().GetResult();
+            if (!connected)
+                Assert.Fail("Connect Fail");
+            //(bool success, string message) = charger.SetFV(27.6).GetAwaiter().GetResult();
+            //Console.WriteLine($"FV value write result={success},{message}");
+            charger.ReadCharger().GetAwaiter().GetResult();
+            charger.DisConnect();
+            charger.Dispose();
+            Console.WriteLine(charger.Datas.ToJson());
+            Assert.IsTrue(charger.Datas.Vin > 200);
+
+        }
+    }
+}
